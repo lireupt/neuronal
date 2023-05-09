5a75aa45624dd9ec378fd0d91570d2e7671e1c61
Fábio <fabiodu465@gmail.com>
2023-05-09 23:01:24 +0100

    mudança da escala X

--- test5.py
+++ test5.py
@@ -19,6 +19,16 @@ scaler_X = MinMaxScaler()
 scaler_y = MinMaxScaler()
 data[['Consumption']] = scaler_y.fit_transform(data[['Power1']])
 data[['daycode']] = scaler_X.fit_transform(data[['daycode']])
+week2 = []
+
+class week:
+  day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+  code = ["0.05", "0.10", "0.15", "0.20", "0.25", "0.30", "0.35"]
+
+for x in data['DayCode1']:
+    for y in week.code:
+        if str(x) == y:
+            week2.append(week.day[week.code.index(y)])
 
 # Prepare the data for training and testing
 lookback = 96  # 24 hours * 4 (15-minute intervals per hour)
@@ -58,6 +68,7 @@ plt.plot(y_pred[-48*2:, 0], label='Predicted')
 plt.title('Electricity Consumption Prediction for Next 2 Days')
 plt.xlabel('Time (15-minute intervals)')
 plt.ylabel('Electricity Consumption')
+plt.xticks(np.arange(0, 100, 15))
 plt.legend()
 plt.show()
 
